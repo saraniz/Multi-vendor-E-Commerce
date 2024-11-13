d063a0cca3ab8df4a612fe3076091c94eb6ce47c
Ravindu112 <141537229+Ravindu112@users.noreply.github.com>
2024-11-13 14:12:52 +0530

    updeted CartPage
    Hi amindiii

---
+++ Frontend/src/Components/Body/Cart/CartSummary.jsx
@@ -0,0 +1,29 @@
+// CartSummary.js
+import React from 'react';
+
+const CartSummary = ({ subtotal, serviceCharge, taxes, total }) => {
+  return (
+    <div className="w-1/4 p-4 bg-gray-200 rounded-lg">
+      <h2 className="mb-4 text-lg font-semibold">Summary</h2>
+      <div className="flex justify-between mb-2">
+        <span>Subtotal</span>
+        <span>Rs.{subtotal}</span>
+      </div>
+      <div className="flex justify-between mb-2">
+        <span>Service Charge</span>
+        <span>Rs.{serviceCharge}</span>
+      </div>
+      <div className="flex justify-between mb-2">
+        <span>Taxes</span>
+        <span>Rs.{taxes}</span>
+      </div>
+      <hr className="my-2" />
+      <div className="flex justify-between text-xl font-bold">
+        <span>Total</span>
+        <span>Rs.{total}</span>
+      </div>
+    </div>
+  );
+};
+
+export default CartSummary;
---
+++ Frontend/src/Pages/CartPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../Components/Header/Navbar'
+import Cart from '../Components/Body/Cart/Cart'
+
+function CartPage() {
+  return (
+    <div><Navbar/>
+    <Cart/>
+    </div>
+  )
+}
+
+export default CartPage
