a95a0aa8dc77b0d379e156d8adc22162263fd42f
saraniz <158010091+saraniz@users.noreply.github.com>
2024-11-15 12:42:07 +0530

    state management create

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,26 @@
+import { useEffect } from 'react';
 import './App.css';
 import HomePage from './Pages/HomePage';
+import {useDispatch, useSelector} from 'react-redux'
+import { Navigate } from 'react-router-dom';
 
 
 function App() {
+
+  //get tokens
+  const jwt = localStorage.getItem('jwt')
+  const {auth} = useSelector(store=>store)
+  const dispatch =useDispatch();
+
+  useEffect(()=>{
+  if(jwt){
+    dispatch(getUserProfile(jwt))
+    Navigate("/")
+  }
+  },[auth.jwt])
+
+
+
   return (
     <div>
       <HomePage/>
