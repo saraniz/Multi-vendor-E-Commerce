572f7c4837d5d2d7327389337ae14ea3aeb1875b
Dunith Munasinghe <113714394+Dunith-M@users.noreply.github.com>
2025-01-04 14:26:11 +0530

    /product-Image        Routes

--- backend/src/routes/productRoutes.js
+++ backend/src/routes/productRoutes.js
@@ -10,4 +10,5 @@ router.put('/update/:id',authenticate,roleMiddleware(['seller']),updateProduct) 
 router.delete('/delete/:id',authenticate,roleMiddleware(['seller']),deleteProduct) //delete product
 router.get('/my-products',authenticate,roleMiddleware(['seller']),getSellerProduct)  //get seller products
 // 👇👇 product image upload
-router.post('/product-image-update',authenticate, productController.product_image_update)
+//router.post('/product-image-update',authenticate, productController.productI)
+router.post('/product-image-upload',authenticate, roleMiddleware(['seller']), addProductImg);
