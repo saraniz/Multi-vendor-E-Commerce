9ac96f0ca760ff3ad7c2c3bb45203e3100c287d9
Dunith Munasinghe <113714394+Dunith-M@users.noreply.github.com>
2025-01-12 11:52:58 +0530

    seller, customer dashboard

--- backend/src/controller/dashboardController.js
+++ backend/src/controller/dashboardController.js
@@ -5,12 +5,15 @@ const userModel = require('../models/users');
 const productModel = require('../models/product');
 const cartModel = require('../models/Cart');
 
+
+responseReturn = (res,code,data) => {
+  return res.status(code).json(data);
+}
+
 const getAdminDashboard = async (req, res) => {
     const { id } = req;
 
-    responseReturn = (res,code,data) => {
-        return res.status(code).json(data);
-    }
+    
 
     try {
         
@@ -40,4 +43,60 @@ const getAdminDashboard = async (req, res) => {
 }
 
 
-module.exports = {getAdminDashboard}
+const getSellerDashboard = async (req, res) => {
+    const { id } = req;
+
+  //   responseReturn = (res,code,data) => {
+  //     return res.status(code).json(data);
+  // }
+
+  // Seller Model isnot available 🔴
+
+}
+
+
+const getCustomerDashboard = async (req, res) => {
+  const { id } = req;
+
+  try{
+    const totalOrders = await prisma.order.count({
+      where: {
+          userId: id,
+      },
+  });
+
+  // Count pending orders for the customer
+  const pendingOrders = await prisma.order.count({
+      where: {
+          userId: id,
+          status: 'Pending',
+      },
+  });
+
+  const totalSpentResult = await prisma.order.aggregate({
+    _sum: {
+        totalPrice: true,
+    },
+    where: {
+        userId: id,
+    },
+});
+  const totalSpent = totalSpentResult._sum.totalPrice || 0;
+
+
+
+  res.status(200).json({
+    totalOrders,
+    pendingOrders,
+    totalSpent,
+
+});
+
+
+  } catch (error) {
+    console.error(error.message);
+    responseReturn(res, 500, { error: 'Internal server error' });
+  }
+}
+
+module.exports = {getAdminDashboard, getSellerDashboard, getCustomerDashboard}
--- backend/src/routes/dashboardRoutes.js
+++ backend/src/routes/dashboardRoutes.js
@@ -5,9 +5,10 @@ const roleMiddleware = require('../middleware/roleMiddleware')
 
 const router = express.Router()
 
-router.get('/get-Admin-dashboardData',authenticate, getAdminDashboard);
+//router.get('/get-Admin-dashboardData',authenticate, getAdminDashboard);
 
-
+//router.get('get-Seller-dashboardData',authenticate, roleMiddleware(['seller']), getSellerDashboard);
+//router.get('get-Customer-dashboardData',authenticate, roleMiddleware(['user']), getCustomerDashboard);
 
 
 
