2c53555163f4855133d1da28c2d5cc8925a35fad
saraniz <158010091+saraniz@users.noreply.github.com>
2025-02-24 10:50:21 +0530

    simple change

--- Frontend/src/Components/ProductPage/ProductDetails.jsx
+++ Frontend/src/Components/ProductPage/ProductDetails.jsx
@@ -27,7 +27,7 @@ const ProductDetails = () => {
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error}</p>;
-  if (!products) return <p>Product not found</p>;
+  if (!products || !products.product) return <p>Product not found</p>;
 
 
   return (
