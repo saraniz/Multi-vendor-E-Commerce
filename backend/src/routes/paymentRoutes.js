7977e61da75b911a4017c66ff7aec1a80faa7225
Dunith Munasinghe <113714394+Dunith-M@users.noreply.github.com>
2025-07-13 17:04:23 +0530

    chage

--- backend/src/routes/paymentRoutes.js
+++ backend/src/routes/paymentRoutes.js
@@ -2,12 +2,13 @@ const express = require('express')
 const { payForSingleProduct, checkoutCart} = require('../controller/paymentController');
 const authenticate = require('../middleware/authMiddleware')
 const roleMiddleware = require('../middleware/roleMiddleware')
-
+const webhookController = require('../controller/webhookController');
 const router = express.Router()
 
 
 
 router.post('/pay-now', payForSingleProduct);
 router.post('/checkout-cart', checkoutCart);
+router.post('/webhook', webhookController);
 
 module.exports = router;
